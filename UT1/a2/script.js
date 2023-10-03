c3b0b37984f4c8b822346ab01c8290a3a5a57f4a
Alejandro Hdez <alherdom@outlook.com>
2023-10-03 18:41:57 +0100

    JS excercise

--- UT1/a2/script.js
+++ UT1/a2/script.js
@@ -2,3 +2,13 @@ function diAlgo()
     {
     alert("Esta todo bien");
     }
+
+function checkAnswer(answer, id) {
+    var question = document.getElementById(id);
+    if (answer == 'True') {
+        question.style.color = 'green';
+    } else {
+        question.style.color = 'red';
+    }
+
+    }
