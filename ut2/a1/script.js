66b535aa9ad8a588fb9b33c97872982eaaae348e
alherdom <alherdom@outlook.com>
2023-10-11 01:13:52 +0100

    Complete excercises 12

--- ut2/a1/script.js
+++ ut2/a1/script.js
@@ -2,7 +2,6 @@
 var fixedRadius = 5;
 var fixedArea = Math.PI * Math.pow(fixedRadius, 2);
 var fixedPerimeter = 2 * Math.PI * fixedRadius;
-
 // Display the results with the fixed radius in the HTML page
 document.getElementById('area1').innerHTML = fixedArea.toFixed(2);
 document.getElementById('perimeter1').innerHTML = fixedPerimeter.toFixed(2);
@@ -10,17 +9,14 @@ document.getElementById('perimeter1').innerHTML = fixedPerimeter.toFixed(2);
 function calculateCircumference() {
     // Get the value of the radius entered by the user
     var radius = parseFloat(document.getElementById('radiusInput').value);
-
     // Check if the radius is a valid number
     if (isNaN(radius)) {
         // Display an error message if the radius is not a valid number
         window.alert("The radius is not a valid number");
     } else {
-
         // Calculate the area and perimeter of the circumference with the entered radius
         var area = Math.PI * Math.pow(radius, 2);
         var perimeter = 2 * Math.PI * radius;
-
         // Display the results in the HTML page
         document.getElementById('area2').innerHTML = area.toFixed(2);
         document.getElementById('perimeter2').innerHTML = perimeter.toFixed(2);
@@ -89,7 +85,6 @@ function getFibonacci() {
     }
     // Log the Fibonacci sequence to the console.
     console.log(fibonacci);
-
     // Display the Fibonacci sequence in the 'fibonacci' element on the HTML page.
     document.getElementById('fibonacci').innerHTML = fibonacci.join(', ');
 }
