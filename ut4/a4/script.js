a705bf78f9f73b9b69e241fa61efad87b5387b41
Alejandro Hdez <alherdom@outlook.com>
2023-11-06 20:29:42 +0000

    Continue the a4 activity

--- ut4/a4/script.js
+++ ut4/a4/script.js
@@ -1,5 +1,5 @@
-function randomArray(length = 20, min = 20, max = 100) {
+function randomArray(length = 15, min = -10, max = 20) {
     const numbers = Array.from({ length }, () => (Math.floor(Math.random() * (max - min)) + min))
     console.log(numbers)
-    document.getElementById('array-numbers').innerHTML = numbers.sort(function (a, b) { return Math.random() }).join(', ');
+    document.getElementById('array-numbers').innerHTML = numbers.join(', ');
 }
