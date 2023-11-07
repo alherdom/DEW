1e38f59d35cee55dd0f0cafa867ac83d69728cb2
Alejandro Hdez <alherdom@outlook.com>
2023-11-07 18:39:28 +0000

    Continue the a4 activity

--- ut4/a4/script.js
+++ ut4/a4/script.js
@@ -1,5 +1,21 @@
 function randomArray(length = 15, min = -10, max = 20) {
-    const numbers = Array.from({ length }, () => (Math.floor(Math.random() * (max - min)) + min))
-    console.log(numbers)
-    document.getElementById('array-numbers').innerHTML = numbers.join(', ');
+  const numbers1 = Array.from(
+    { length },
+    () => Math.floor(Math.random() * (max - min)) + min
+  );
+  console.log(numbers1);
+  document.getElementById("array-numbers").innerHTML = numbers1.join(", ");
+  const numbers2 = numbers1;
+  for (i = 0; i < length; i++) {
+    const x = numbers2[i];
+    if (x <= -5) {
+      console.log(x, "Less or equal to -5");
+    } else if (-5 < x && x <= 0) {
+      console.log(x, "Between -5 and 0");
+    } else if (0 < x && x <= 10) {
+      console.log(x, "Between 0 and 10");
+    } else if (10 < x && x <= 10) {
+      console.log(x, "Between 10 and 10");
+    }
+  }
 }
