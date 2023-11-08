73c72095eb0e9123819907ec12ac0f20ec067e66
Alejandro Hdez <alherdom@outlook.com>
2023-11-08 15:45:41 +0000

    Complete a6 ut4

--- ut4/a5/script.js
+++ ut4/a5/script.js
@@ -1,3 +1,6 @@
+function someFunctions() {
+  randomArray();
+}
 function randomArray(length = 20, min = 60, max = 100) {
   const numbers1 = Array.from(
     { length },
@@ -5,11 +8,14 @@ function randomArray(length = 20, min = 60, max = 100) {
   );
   console.log(numbers1);
   document.getElementById("array-numbers").innerHTML = numbers1.join(", ");
-  const numbers2 = new Array();
-  for (i = 0; i < length; i++) {
-    let x = numbers1[i];
-    numbers2.push(Math.sqrt(x).toFixed(2));
+  squareArray(numbers1);
+  function squareArray(array) {
+    const numbers2 = new Array();
+    for (i = 0; i < length; i++) {
+      const x = array[i];
+      numbers2.push(Math.sqrt(x).toFixed(2));
+    }
+    console.log(numbers2);
+    document.getElementById("array-numbers2").innerHTML = numbers2.join(", ");
   }
-  console.log(numbers2);
-  document.getElementById("array-numbers2").innerHTML = numbers2.join(", ");
 }
---
+++ ut4/a6/script.js
@@ -0,0 +1,20 @@
+function randomArray(length = 20, min = -100, max = 100) {
+  const numbers = Array.from(
+    { length },
+    () => Math.floor(Math.random() * (max - min)) + min
+  );
+  console.log(numbers);
+  document.getElementById("array-numbers").innerHTML = numbers.join(", ");
+  let max_value = numbers.reduce(function (a, b) {
+    return Math.max(a, b);
+  }, -Infinity);
+  let min_value = numbers.reduce(function (a, b) {
+    return Math.min(a, b);
+  }, +Infinity);
+  console.log(max_value);
+  console.log(min_value);
+  document.getElementById("max-value").innerHTML = max_value;
+  document.getElementById("max-index").innerHTML = numbers.indexOf(max_value);
+  document.getElementById("min-value").innerHTML = min_value;
+  document.getElementById("min-index").innerHTML = numbers.indexOf(min_value);
+}
