0571b24df596aa03e8163b7823a9fbf41526e9f5
Alejandro Hdez <alherdom@outlook.com>
2023-12-15 20:23:37 +0000

    ToDo: refact script to drawing board.

---
+++ ut6/drawing_board/script.js
@@ -0,0 +1,54 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const cellNumber = 35;
+  var board = document.getElementById("zonadibujo");
+  var boardTable = document.createElement("table");
+  boardTable.className = "tablerodibujo";
+  board.appendChild(boardTable);
+  for (var i = 0; i < cellNumber; i++) {
+    var row = document.createElement("tr");
+    boardTable.appendChild(row);
+    for (var j = 0; j < cellNumber; j++) {
+      var cell = document.createElement("td");
+      cell.addEventListener("click", function () {
+        this.style.backgroundColor = pincel.style.backgroundColor;
+      });
+      row.appendChild(cell);
+    }
+  }
+});
+
+const pincel = document.getElementById("pincel");
+document.getElementById("color1").addEventListener("click", function () {
+  pincel.style.backgroundColor = "#FF0";
+  pincel.style.color = "black";
+  
+});
+
+document.getElementById("color2").addEventListener("click", function () {
+  pincel.style.backgroundColor = "#0F0";
+  pincel.style.color = "black";
+});
+
+document.getElementById("color3").addEventListener("click", function () {
+  pincel.style.backgroundColor = "#000";
+  pincel.style.color = "white";
+});
+
+document.getElementById("color4").addEventListener("click", function () {
+  pincel.style.backgroundColor = "#F00";
+  pincel.style.color = "black";
+});
+
+document.getElementById("color5").addEventListener("click", function () {
+  pincel.style.backgroundColor = "#06C";
+  pincel.style.color = "black";
+});
+
+document.getElementById("color6").addEventListener("click", function () {
+  pincel.style.backgroundColor = "#FFF";
+  pincel.style.color = "black";
+});
+
+document.querySelector("#btnLimpiar").addEventListener("click", () => {
+  window.location.reload();
+});
