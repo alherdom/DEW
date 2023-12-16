fccd824a7b3a1532e4c38682e68b35cd6082bef7
Alejandro Hernández <alherdom@outlook.com>
2023-12-16 18:37:56 +0000

    ToDo: create select color function.

--- ut6/drawing_board/script.js
+++ ut6/drawing_board/script.js
@@ -1,54 +1,133 @@
+let isPainting = false;
+const pincel = document.getElementById("pincel");
+
+function startPainting() {
+  isPainting = true;
+  pincel.textContent = "🖌️ ACTIVADO";
+}
+
+function stopPainting() {
+  isPainting = false;
+  pincel.textContent = "🖌️ DESACTIVADO";
+}
+
+function paint(event) {
+  if (isPainting) {
+    event.target.style.backgroundColor = pincel.style.backgroundColor;
+  }
+}
+
 document.addEventListener("DOMContentLoaded", function () {
-  const cellNumber = 100;
-  var board = document.getElementById("zonadibujo");
-  var boardTable = document.createElement("table");
+  const cellNumber = 90;
+  const board = document.getElementById("zonadibujo");
+  const boardTable = document.createElement("table");
   boardTable.className = "tablerodibujo";
   board.appendChild(boardTable);
   for (var i = 0; i < cellNumber; i++) {
-    var row = document.createElement("tr");
+    const row = document.createElement("tr");
     boardTable.appendChild(row);
     for (var j = 0; j < cellNumber; j++) {
-      var cell = document.createElement("td");
-      cell.addEventListener("mousemove", function () {
-        this.style.backgroundColor = pincel.style.backgroundColor;
-      });
+      const cell = document.createElement("td");
+      cell.addEventListener("mousemove", paint);
+      cell.addEventListener("mousedown", startPainting);
+      cell.addEventListener("mouseup", stopPainting);
       row.appendChild(cell);
     }
   }
 });
 
-const pincel = document.getElementById("pincel");
-document.getElementById("color1").addEventListener("click", function () {
-  pincel.style.backgroundColor = "#FF0";
-  pincel.style.color = "black";
-  
+document.querySelector(".color1").addEventListener("click", function () {
+  pincel.style.backgroundColor = getComputedStyle(this).backgroundColor;
+  this.classList.add("seleccionado");
+  document.querySelector(".color2").classList.remove("seleccionado");
+  document.querySelector(".color3").classList.remove("seleccionado");
+  document.querySelector(".color4").classList.remove("seleccionado");
+  document.querySelector(".color5").classList.remove("seleccionado");
+  document.querySelector(".color6").classList.remove("seleccionado");
+  document.querySelector(".color6").classList.remove("seleccionado");
+  document.querySelector(".color7").classList.remove("seleccionado");
 });
 
-document.getElementById("color2").addEventListener("click", function () {
-  pincel.style.backgroundColor = "#0F0";
-  pincel.style.color = "black";
+document.querySelector(".color2").addEventListener("click", function () {
+  pincel.style.backgroundColor = getComputedStyle(this).backgroundColor;
+  this.classList.add("seleccionado");
+  document.querySelector(".color1").classList.remove("seleccionado");
+  document.querySelector(".color3").classList.remove("seleccionado");
+  document.querySelector(".color4").classList.remove("seleccionado");
+  document.querySelector(".color5").classList.remove("seleccionado");
+  document.querySelector(".color6").classList.remove("seleccionado");
+  document.querySelector(".color7").classList.remove("seleccionado");
 });
 
-document.getElementById("color3").addEventListener("click", function () {
-  pincel.style.backgroundColor = "#000";
-  pincel.style.color = "white";
+document.querySelector(".color3").addEventListener("click", function () {
+  pincel.style.backgroundColor = getComputedStyle(this).backgroundColor;
+  this.classList.add("seleccionado");
+  document.querySelector(".color1").classList.remove("seleccionado");
+  document.querySelector(".color2").classList.remove("seleccionado");
+  document.querySelector(".color4").classList.remove("seleccionado");
+  document.querySelector(".color5").classList.remove("seleccionado");
+  document.querySelector(".color6").classList.remove("seleccionado");
+  document.querySelector(".color7").classList.remove("seleccionado");
 });
 
-document.getElementById("color4").addEventListener("click", function () {
-  pincel.style.backgroundColor = "#F00";
-  pincel.style.color = "black";
+document.querySelector(".color4").addEventListener("click", function () {
+  pincel.style.backgroundColor = getComputedStyle(this).backgroundColor;
+  this.classList.add("seleccionado");
+  document.querySelector(".color1").classList.remove("seleccionado");
+  document.querySelector(".color2").classList.remove("seleccionado");
+  document.querySelector(".color3").classList.remove("seleccionado");
+  document.querySelector(".color5").classList.remove("seleccionado");
+  document.querySelector(".color6").classList.remove("seleccionado");
+  document.querySelector(".color7").classList.remove("seleccionado");
 });
 
-document.getElementById("color5").addEventListener("click", function () {
-  pincel.style.backgroundColor = "#06C";
-  pincel.style.color = "black";
+document.querySelector(".color5").addEventListener("click", function () {
+  pincel.style.backgroundColor = getComputedStyle(this).backgroundColor;
+  this.classList.add("seleccionado");
+  document.querySelector(".color1").classList.remove("seleccionado");
+  document.querySelector(".color2").classList.remove("seleccionado");
+  document.querySelector(".color3").classList.remove("seleccionado");
+  document.querySelector(".color4").classList.remove("seleccionado");
+  document.querySelector(".color6").classList.remove("seleccionado");
+  document.querySelector(".color7").classList.remove("seleccionado");
 });
 
-document.getElementById("color6").addEventListener("click", function () {
-  pincel.style.backgroundColor = "#FFF";
-  pincel.style.color = "black";
+document.querySelector(".color6").addEventListener("click", function () {
+  pincel.style.backgroundColor = getComputedStyle(this).backgroundColor;
+  this.classList.add("seleccionado");
+  document.querySelector(".color1").classList.remove("seleccionado");
+  document.querySelector(".color2").classList.remove("seleccionado");
+  document.querySelector(".color3").classList.remove("seleccionado");
+  document.querySelector(".color4").classList.remove("seleccionado");
+  document.querySelector(".color5").classList.remove("seleccionado");
+  document.querySelector(".color7").classList.remove("seleccionado");
+});
+
+document.querySelector(".color7").addEventListener("click", function () {
+  pincel.style.backgroundColor = getComputedStyle(this).backgroundColor;
+  this.classList.add("seleccionado");
+  document.querySelector(".color1").classList.remove("seleccionado");
+  document.querySelector(".color2").classList.remove("seleccionado");
+  document.querySelector(".color3").classList.remove("seleccionado");
+  document.querySelector(".color4").classList.remove("seleccionado");
+  document.querySelector(".color5").classList.remove("seleccionado");
+  document.querySelector(".color6").classList.remove("seleccionado");
+});
+
+document.querySelector(".color8").addEventListener("click", function () {
+  pincel.style.backgroundColor = getComputedStyle(this).backgroundColor;
+  document.querySelector(".color1").classList.remove("seleccionado");
+  document.querySelector(".color2").classList.remove("seleccionado");
+  document.querySelector(".color3").classList.remove("seleccionado");
+  document.querySelector(".color4").classList.remove("seleccionado");
+  document.querySelector(".color5").classList.remove("seleccionado");
+  document.querySelector(".color7").classList.remove("seleccionado");
 });
 
 document.querySelector("#btnLimpiar").addEventListener("click", () => {
   window.location.reload();
+});
+
+document.body.addEventListener("dragstart", function (event) {
+  event.preventDefault();
 });
